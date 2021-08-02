bf5097343800f297fd5cafe6f0a5aeca3a96b649
aliciachamar <aliciachamar@gmail.com>
2021-08-02 13:25:02 -0600

    added handlebars to server.js

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require('../config/connection');
 
-const User extends Model {}
+class User extends Model {}
 
 User.init(
     {
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const session = require("express-session");
 const exphbs = require("express-handlebars");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const path = require("path");
+const hbs = exphbs.create({});
 
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
@@ -12,6 +13,9 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Set up the session with cookies
+
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
