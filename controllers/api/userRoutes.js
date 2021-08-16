e48f7da8d664bebedd3d853f891eb7067ff39eaa
aliciachamar <aliciachamar@gmail.com>
2021-08-16 11:31:44 -0600

    fixing heroku login problem

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,40 +2,6 @@ const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.get("/dashboard", withAuth, async (req, res) => {
-    try {
-        const userPosts = await Post.findAll({
-            where: {
-                userId: req.session.user_id
-            }
-        });
-        const posts = userPosts.map(post => post.toJSON());
-        res.status(200).render("dashboard", {
-            posts,
-            logged_in: req.session.logged_in
-        });
-    } catch (e) {
-        console.log(e);
-        res.status(500).json(e);
-    }
-});
-
-router.get("/login", (req, res) => {
-    if (req.session.logged_in) {
-        res.status(200).redirect("/dashboard");
-    }
-
-    res.status(200).render("login");
-});
-
-router.get("/register", (req, res) => {
-    if (req.session.logged_in) {
-        res.status(200).redirect("/dashboard");
-    }
-
-    res.status(200).render("register");
-});
-
 router.post('/register', async (req, res) => {
     try {
       const userData = await User.create(req.body);
@@ -43,19 +9,20 @@ router.post('/register', async (req, res) => {
         req.session.user_id = userData.id;
         req.session.logged_in = true;
       });
-      const allPosts = await Post.findAll({
-        where: {
-          userId: userData.id
-        },
-        include: [{
-            model: Comment,
-            include: [User]
-        }, User]
-        });
-        const posts = allPosts.map(post => post.toJSON());
-        res.status(200).render("dashboard", { 
-          posts,
-          logged_in: req.session.logged_in });
+      res.redirect("/dashboard");
+      // const allPosts = await Post.findAll({
+      //   where: {
+      //     userId: userData.id
+      //   },
+      //   include: [{
+      //       model: Comment,
+      //       include: [User]
+      //   }, User]
+      //   });
+      //   const posts = allPosts.map(post => post.toJSON());
+      //   res.status(200).render("dashboard", { 
+      //     posts,
+      //     logged_in: req.session.logged_in });
     } catch (e) {
       console.log(e);
       res.status(500).json(e);
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -20,4 +20,37 @@ router.get("/", async (req, res) => {
     }
 });
 
+router.get("/dashboard", withAuth, async (req, res) => {
+    try {
+        const userPosts = await Post.findAll({
+            where: {
+                userId: req.session.user_id
+            }
+        });
+        const posts = userPosts.map(post => post.toJSON());
+        res.status(200).render("dashboard", {
+            posts,
+            logged_in: req.session.logged_in
+        });
+    } catch (e) {
+        console.log(e);
+        res.status(500).json(e);
+    }
+});
+
+router.get("/login", (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect("/dashboard");
+    }
+    res.status(200).render("login");
+});
+
+router.get("/register", (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect("/dashboard");
+    }
+
+    res.status(200).render("register");
+});
+
 module.exports = router;
